8533137a53a695d26bc9415a5357508a11cf199e
hzh <3281714703@qq.com>
2025-07-01 16:56:04 +0800

    hzh

---
+++ src/main/java/com/machinery/mall/mapper/OrderMapper.java
@@ -0,0 +1,15 @@
+package com.machinery.mall.mapper;
+
+import com.machinery.mall.entity.Order;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface OrderMapper {
+    int insertOrder(Order order);
+    Order selectOrderById(@Param("id") Integer id);
+    List<Order> selectOrdersByUserId(@Param("uid") Integer uid);
+    // 可根据需要添加更多方法
+} 
